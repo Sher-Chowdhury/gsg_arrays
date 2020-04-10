8d799a844741b904ca96417c2486509f349debb9
Sher-Chowdhury <sherc1980@yahoo.com>
2020-04-10 12:13:53 +0100

    wip

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 )
 
 func main() {
@@ -16,7 +17,17 @@ func main() {
 	fmt.Println(myArray)
 	fmt.Println(myArray[2])
 
+	// Here's another syntax
 	my2ndArray := [3]int{18, 25, 30}
 	fmt.Println(my2ndArray)
 
+	// here the "..." tells golang to work out the array length for
+	// us based on the number or values provided.
+	// This technique is referred to as array literals.
+	stooges := [...]string{"Moe", "Larry", "Curly", "charlie"}
+	fmt.Println(reflect.TypeOf(stooges)) // [4]string
+	fmt.Println(len(stooges))            // 4
+
 }
+
+// https://yourbasic.org/golang/three-dots-ellipsis/
